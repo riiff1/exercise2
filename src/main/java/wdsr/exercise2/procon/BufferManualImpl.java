7d8ef04c06f42a9a16b32c34329d38b14f7a427d
riiff1 <riiff@wp.pl>
2017-03-22 23:45:25 +0100

    finish task: "procon"

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -1,16 +1,24 @@
 package wdsr.exercise2.procon;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Task: implement Buffer interface without using any *Queue classes from java.util.concurrent package.
  * Any combination of "synchronized", *Lock, *Semaphore, *Condition, *Barrier, *Latch is allowed.
  */
 public class BufferManualImpl implements Buffer {
-	public void submitOrder(Order order) throws InterruptedException {
+
+	private Queue<Order> orders = new LinkedList<>();
+
+	public synchronized void submitOrder(Order order) throws InterruptedException {
 		// TODO
+		orders.add(order);
 	}
 	
-	public Order consumeNextOrder() throws InterruptedException {
+	public synchronized Order consumeNextOrder() throws InterruptedException {
 		// TODO
-		return null;
+		return orders.poll();
 	}
 }
--- src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
@@ -1,15 +1,21 @@
 package wdsr.exercise2.procon;
 
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
 /**
  * Task: implement Buffer interface using one of *Queue classes from java.util.concurrent package.
  */
 public class BufferQueueImpl implements Buffer {
+
+	private BlockingDeque queue = new LinkedBlockingDeque();
 	public void submitOrder(Order order) throws InterruptedException {
 		// TODO
+		queue.add(order);
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
 		// TODO
-		return null;
+		return (Order) queue.remove();
 	}
 }
