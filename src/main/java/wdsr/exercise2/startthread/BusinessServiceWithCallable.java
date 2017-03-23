c30cf55e30c13f750aa6336448f449043a415a6e
riiff1 <riiff@wp.pl>
2017-03-23 08:33:46 +0100

    fix callable

--- src/main/java/wdsr/exercise2/startthread/BusinessServiceWithCallable.java
+++ src/main/java/wdsr/exercise2/startthread/BusinessServiceWithCallable.java
@@ -1,5 +1,6 @@
 package wdsr.exercise2.startthread;
 
+import java.util.ArrayList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -8,6 +9,7 @@ import java.util.concurrent.Future;
 public class BusinessServiceWithCallable {
 	private final ExecutorService executorService;	
 	private final NumericHelper helper;
+	ArrayList<Future<Integer>> futures;
 	
 	public BusinessServiceWithCallable(ExecutorService executorService, NumericHelper helper) {
 		this.executorService = executorService;
@@ -22,7 +24,7 @@ public class BusinessServiceWithCallable {
 	 */
 	public long sumOfRandomInts() throws InterruptedException, ExecutionException {	
 		long result = 0;
-		
+		futures = new ArrayList<>(100);
 		// TODO Task: 
 		// 1. create 100 Callable objects that invoke helper.nextRandom in their call() method.
 		Callable<Integer> callable = new Callable<Integer>() {
@@ -35,8 +37,14 @@ public class BusinessServiceWithCallable {
 		{
 			// 2. submit all Callable objects to executorService (executorService.submit or executorService.invokeAll)
 			Future<Integer> future = executorService.submit(callable);
+			futures.add(future);
 			// 3. sum up the results - each random number can be retrieved using future.get() method.
+		}
+		//trzeba zrobic druga petle ktora iteruje sie po kolekcji futurow i dla kazdego futura wola get
+		for(Future<Integer> future : futures)
+		{
 			result += future.get();
+
 		}
 		// 4. return the computed result.
 		return result;
